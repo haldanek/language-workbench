371789ef5d3aeab7bf864a30a37dc838e4dda9a1
Anthony D. Mays <anthony@morganlatimer.com>
2024-02-16 21:39:29 +0000

    feat: adds html starter

---
+++ html-css-javascript/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  printWelcomeMessage();
+});
+
+function printWelcomeMessage() {
+  console.info("🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥");
+  console.info("🔥 Welcome to the Code Differently course! 🎉");
+  console.info("🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥🎉🔥");
+}
